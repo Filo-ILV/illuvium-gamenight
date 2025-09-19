5f3ef627bc16faf34ecc3f66f5e6ee7ad73e1c01
Filo <pedro.nunes@illuvium.io>
2025-09-19 21:09:35 +0100

    Update gamenight-overview.tsx
    Edited ILV amounts.

--- src/components/gamenight-overview.tsx
+++ src/components/gamenight-overview.tsx
@@ -174,7 +174,7 @@ export function GamenightOverview() {
               className="h-full"
             >
               <div className="space-y-3">
-                <p>Each community will receive a budget of <span className="text-primary font-bold">20 ILV (Illuvium token)</span>.</p>
+                <p>Each community will receive a budget of <span className="text-primary font-bold">10-30 ILV (Illuvium token)</span> according to the number of players participating in the Game Night.</p>
                 <p><strong>Additional:</strong> Illuvitars will also be awarded to participants.</p>
               </div>
             </InfoCard>
